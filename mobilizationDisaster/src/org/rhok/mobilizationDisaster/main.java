9d68c6b70e0fc8cfcf794f8a7f509fb37a3f8141
Uwe Kamper <me@uwekamper.de>
2011-06-04 19:26:56 +0200

    Added contacts-gatherer class

--- mobilizationDisaster/src/org/rhok/mobilizationDisaster/main.java
+++ mobilizationDisaster/src/org/rhok/mobilizationDisaster/main.java
@@ -7,7 +7,10 @@ public class main extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        mContacts = new ContactsGatherer(getContentResolver());
+    	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    
+    public ContactsGatherer mContacts;
 }
