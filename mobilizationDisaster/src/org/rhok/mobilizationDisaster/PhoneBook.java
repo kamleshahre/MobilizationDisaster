5a2021be8a2e9617566e8ab8f1fe7cda4a9b7ba7
Uwe Kamper <me@uwekamper.de>
2011-06-04 20:21:33 +0200

    Rename of ContactsGatherer to PhoneBook

---
+++ mobilizationDisaster/src/org/rhok/mobilizationDisaster/PhoneBook.java
@@ -0,0 +1,76 @@
+package org.rhok.mobilizationDisaster;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+
+public class PhoneBook {
+	/**
+	 * Default constructor
+	 */
+	public PhoneBook(ContentResolver cr) {
+		this.cr = cr;
+	}
+	
+	/**
+	 * Get the all the phone numbers from a group
+	 */
+	public List<PhoneBookEntry> getEverything() {
+		List<PhoneBookEntry> entries = new ArrayList<PhoneBookEntry>();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        if(cur.getCount() > 0) {
+        	while(cur.moveToNext()) {
+        		String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+        		String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+        		if (cur.getInt(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+        			entries.add( new PhoneBookEntry(id, name, getPhoneNumberById(id)) );
+        		}
+            }
+        }
+        cur.close();
+		return entries;
+	}
+	
+	/**
+	 * Get all the entries for a particular group
+	 */
+	public List<PhoneBookEntry> getGroup(String groupName) {
+		List<PhoneBookEntry> entries = new ArrayList<PhoneBookEntry>();
+		Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        if(cur.getCount() > 0) {
+        	while(cur.moveToNext()) {
+        		boolean hasPhoneNumber = 
+        			(cur.getInt(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0);
+        		
+        		if(hasPhoneNumber) {
+        			String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+            		String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+        			entries.add(new PhoneBookEntry(id, name, getPhoneNumberById(id)));
+        		}
+            }
+        }
+        cur.close();
+		return entries;
+	}
+	
+	/**
+	 * Queries a phone number by id
+	 */
+	public String getPhoneNumberById(String id) {
+		String number = null;
+		Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, 
+			null, 
+ 		    ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?", 
+ 		    new String[]{id}, null);
+ 	    while (pCur.moveToFirst()) {
+ 	    	number = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+ 	    } 
+        pCur.close();
+        return number;
+	}
+	// members
+	public ContentResolver cr;
+}
--- mobilizationDisaster/src/org/rhok/mobilizationDisaster/main.java
+++ mobilizationDisaster/src/org/rhok/mobilizationDisaster/main.java
@@ -7,10 +7,12 @@ public class main extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        mContacts = new ContactsGatherer(getContentResolver());
+        m_contacts = new PhoneBook(getContentResolver());
+        m_contacts.getEverything();
+        m_contacts.getGroup("responders");
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
     
-    public ContactsGatherer mContacts;
+    public PhoneBook m_contacts;
 }
